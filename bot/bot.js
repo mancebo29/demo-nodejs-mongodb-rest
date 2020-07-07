c0881a7e66b81ae46185d3ef895e36a60fe1a4ea
Jairo Mancebo <jairo.mancebo@helloalfred.com>
2020-07-07 17:24:28 -0400

    Attempt 1

--- bot/bot.js
+++ bot/bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var mongodb       = require('../db');
+
 
 module.exports = function setUpBot () {
 
@@ -8,8 +10,24 @@ module.exports = function setUpBot () {
     });
 
     client.on('message', message => {
-        if (message.content === 'henlo') {
-            message.channel.send('Hello there.');
+        if (message.content === '!movies') {
+            mongodb.seeQueue().then(movies => {
+               let reply = 'Las películas en queue son: ';
+               movies.forEach(m => reply += '\n' + `${m.name} (${m.year})`);
+               message.channel.send(reply);
+            });
+        }
+
+        if (message.content.startsWith('!addMovie') || message.content.toLowerCase().startsWith('vamos a ver')) {
+            let title;
+            if (message.content.startsWith('!addMovie')) {
+                title = message.content.substr(9);
+            } else {
+                title = message.content.substr(message.content.indexOf('a ver') + 5);
+            }
+            mongodb.enqueue(title).then(m => {
+                message.channel.send(`Se agregó ${m.name} (${m.year})`)
+            });
         }
     });
 
