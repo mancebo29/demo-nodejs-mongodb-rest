e282f139370c4ec1cbf1fa9adc8c460027f00302
Jairo Mancebo <jairo.mancebo@helloalfred.com>
2020-07-16 12:35:30 -0400

    Add logging

--- services/jokeService.js
+++ services/jokeService.js
@@ -10,6 +10,8 @@ const jokeService = {
     getRandomJoke: async () => {
         const joke = await axios.get(`https://sv443.net/jokeapi/v2/joke/Any`, genericConfig).then(d => d.data);
 
+        console.log('JOKE', JSON.stringify(joke));
+
         return joke;
     }
 };
