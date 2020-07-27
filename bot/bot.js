ccc6ad0f163d07af2d4ddb95149f24c789dcea4b
Jairo Mancebo <jairo.mancebo@helloalfred.com>
2020-07-27 14:23:19 -0400

    Enhace regex further

--- bot/bot.js
+++ bot/bot.js
@@ -22,7 +22,7 @@ module.exports = function setUpBot() {
             }
             return;
         }
-        if (/([\Wa-zNn]N|[\Wa-zNn]Ñ|(^|\W)[nNñÑ])([^a-zA-Z]? *)*[oO0]($|\W|p|[oO0])/g.test(message.content)) {
+        if (/([\Wa-zNn]N|[\Wa-zNn]Ñ|(^|\W)[nNñÑ])([^a-zA-Z]? *)*[oO0οΟ]($|\W|p|[oO0οΟ])/g.test(message.content)) {
             message.delete();
         }
         if (message.author.tag && message.author.tag.endsWith('4806') && Math.random() < 0.34) {
